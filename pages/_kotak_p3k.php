f4e9312a0769e5c771814f65f13eb635907f0f09
dianbny <pratama.dianbny@gmail.com>
2024-11-27 09:00:30 +0800

    Memperbarui Menambah Kolom Kotak P3K

--- pages/_kotak_p3k.php
+++ pages/_kotak_p3k.php
@@ -24,18 +24,41 @@
                 <th>Lokasi Kotak P3K</th>
                 <th style="text-align:center;">No. Kotak P3K</th>
                 <th style="text-align:center;">Tipe Kotak P3K</th>
+                <th style="text-align:center;">Jumlah Item</th>
+                <th style="text-align:center;">Jumlah Isi Kotak</th>
                 <th style="text-align:center;">Detail List Kotak</th>
                 <th colspan="2" style="text-align:center;">Aksi</th>
             </tr>
             <?php
                 if($getData->cekP3K() > 0){
                     $no = 1;
-                    foreach($getData->listKotakP3K() as $row){ ?>
+                    foreach($getData->listKotakP3K() as $row){ 
+                    $jumIsiKotak = $getData->getJumDataIsiKotak($row['_id_kotak']); ?>
                         <tr>
                             <td><?= $no++."."; ?></td>
                             <td><?= $row['_lokasi_kotak']; ?></td>
                             <td style="text-align:center;"><?= $row['_no_kotak']; ?></td>
                             <td style="text-align:center;"><?= $row['_tipe_kotak']; ?></td>
+                            <td style="text-align:center;">
+                                <?php 
+                                    if($getData->cekIsiKotakP3K($row['_id_kotak']) > 0){
+                                        echo $getData->cekIsiKotakP3K($row['_id_kotak']);
+                                    } 
+                                    else { ?>
+                                        <span style="color:red;">0</span>
+                              <?php }
+                                ?>
+                            </td>
+                            <td style="text-align:center;">
+                                <?php
+                                    if($jumIsiKotak['jumlah'] > 0){
+                                        echo $jumIsiKotak['jumlah'];
+                                    }
+                                    else { ?>
+                                        <span style="color:red;">0</span>
+                              <?php }
+                                ?>
+                            </td>
                             <td style="text-align:center;"><a href="detail-data-kotak-<?= $row['_id_kotak']; ?>" class="linkDetail"><i class="fa fa-info-circle" aria-hidden="true"></i></a></td>
                             <td style="text-align:center;"><a href="edit-data-kotak-<?= $row['_id_kotak']; ?>" class="linkDetail"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
                             <td style="text-align:center;"><a href="javascript:void(0)" data-id="<?= $row['_id_kotak']; ?>" class="linkError konfirmDeleteKotakP3K"><i class="fa fa-times-circle" aria-hidden="true"></i></a></td>
@@ -44,7 +67,7 @@
                 }
                 else { ?>
                     <tr>
-                        <td colspan="6" style="text-align:center;"><span style="color:red;">Belum Ada Data Kotak P3K !</span></td>
+                        <td colspan="9" style="text-align:center;"><span style="color:red;">Belum Ada Data Kotak P3K !</span></td>
                     </tr>
           <?php }
             ?>
